cc65a1448e13330811b42f48f1ef4ede88c68b21
hfg-emre-c <72493323+hfg-emre-c@users.noreply.github.com>
2021-02-01 11:43:48 +0100

    Update
    Dateien hochladen

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Tic-Tac-Toe
+# TicTacToe
+Semesteraufgabe - TicTacToe
+
+# Tic - Tac - Toe
+Das Spiel ist Tic - Tac - Toe, man kann gegen einen Freund spielen. Es läuft über den Browser, besitzt einen Runden Counter sowie auch einem Win Counte.
+
+## Usage / Benutzung
+
+Sobald das Dokument heruntergeladen wurde, muss man lediglich die tictactoe1.html datei öffnen. Dieser wird sich in einem Browser öffnen.<br>
+Man erhält die Anweisungen via Pop-Up. Weiterhin wird angezeigt, wer gerade am Zug ist. <br>
+1. Spielernamen eingeben und via "Spielernamen bestätigen" Button bestätigen
+2. Anzahl der Symbole eingeben, wann man gewinnen soll
+2. Feldgröße n x n bestimmen durch den Input Feld und via erstellen button das Spielfeld generieren
+3. Zum spielen bitte abwechselnd die Felder anklicken<br>
+4. Sobald jemand gewonnen (Wenn Spielfeld 3x3 ist - benötigt man 3 Symbole in gleicher Reihenfolge) hat, kann man entweder über das Button "Neue Runde starten" eine neue Runde starten oder via "NEUES SPIEL STARTEN" button ein komplett neues Spiel starten. Hier setzen sich der Win und Rundencounter zurück
+
+## Structure / Aufbau
+
+
+* buttonaktiviere.(): aktiviert den Button für ein Neues Spiel unten im Bereich
+* Spielfeldgröße: wird am anfang vom Spiel angegeben
+* changepplayername(): Ändert ausgaben in HTML, gibt ein PopUp aus und deaktiviert die input Felder von namen
+* changetext(): wechselt den Text wer aktuell am Zug ist
+* rundenzaehler(): zählt round (Rundenzähler) hoch
+* gewinnanzeige(): Gibt aus wer gewonnen hat
+* neustart(): wechselt den Buttontext in "Neue Runde starten"
+* close(): schließt die td's damit man nicht weiter Spielen kann
+* changeplayer(); fragt ab ob der currentPlayer gerade player1 ist, wenn nicht ist der player2 der currentPlayer
+* counter(): die Funktion zählt den Win Counter hoch je nachdem wer gewonnen hat mit der If & else funktion
+
+
+#```generate()``` Diese funktion wird bei onclick auf der HTML verwendet. In generate wird ein counter sowie newId als 0 gesetzt. Dann entnimmt die funktion die Spielfeldgröße mit geElementsById("groeße") die Zahl welches wiederum bei i verwendet wird. Sobald die Tabelle erstellt wird, werden ID's vergeben.
+
+#```$("table").on("click", "td", function()``` Sobald man in die Tabelle klickt, werden die aktionen ausgeführt wie die ID - splitten, status[x][y] vom currentPlayer auslesen und die Funktionen ausgeführt wie  ```changetext()```, ```checkForWin(x,y,currentplayer)``` und ```changeplayer()```. 
+
+#```checkForWin()``` in checkForWin werden die winCheck Funktionen ausgeführt. 
+
+#```wincheckhorizontalvertical()``` Die funktion erhält den letzten Punkt und geht von hier aus auf die Suche - dabei sollen eben die Anzahl der m (Symbole) übereinstimmen. Wenn also m gleich drei ist, und X also das gleiche Zeichen so oft gefunden wird gibt es einen gewinner. Dabei wird die Zeile nach Anzahl vom Spielfeld gesucht - und dementsprechend funktioniert auch die senkrechte Spalte. Nur das hier entweder hoch oder runter kontrolliert wird. Dazu nutzt die funktion mein Array Status, da dort die Symbole eingetragen werden. Falls jemand gewinnt, werden die funktionen wie ```gewinnanzeige()```, ```neustart()```, ```close()```, ```buttonaktivieren()``` & ```counter()``` ausgeführt da diese erst nach einem Win ausgeführt werden sollen. 
+
+#```wincheckquer()``` In der Funktion werden die Diagonalen überprüft - über den Status erhält es wieder die Koordinaten und vergleicht es mit dem M (anforderung von der Anzahl der Symbole). M wird vom Input in der HTML Datei bezogen wie beim wincheckhorizontalvertical(). Falls jemand gewinnt, werden die funktionen wie ```gewinnanzeige()```, ```neustart()```, ```close()```, ```buttonaktivieren()``` & ```counter()``` ausgeführt da diese erst nach einem Win ausgeführt werden sollen. 
---
+++ tictactoe1.js
@@ -0,0 +1,257 @@
+alert("Bitte erst namen festlegen, Spielfeldgröße bestimmen und dann erst den Button zum erstellen betätigen!!");
+let Spielfeldgröße = "";
+
+let currentPlayer = "X";
+let player1 = "X";
+let player2 = "O";
+
+let player1name = "";
+let player2name = "";
+
+
+let player1counter = 0;
+let player2counter = 0;
+let round = 0;
+let status = [];
+
+function buttonaktivieren() {
+    document.getElementById("buttondown").disabled = false;
+}
+//wird in HTML ausgeführt und ändert bzw fügt die namen hinzu
+//gibt PopUp's mit "Spielernamen wurden festgelegt", "Spieler Y ist X", "Spieler X ist O"
+/*jshint ignore:start */
+function changeplayername() {
+    player1name = document.getElementById("player1").value;
+    document.getElementById("player1").disabled = true;
+    player2name = document.getElementById("player2").value;
+    document.getElementById("player2").disabled = true;
+
+    alert("Spielernamen wurden festgelegt");
+    alert(player1name + " ist " + player1);
+    alert(player2name + " ist " + player2);
+
+    document.getElementById("update").innerHTML = player1name + " fängt mit dem Symbol X an sobald Anzahl der Symbole zum win und die Spielfeldgröße bestimmt wurden"
+    var elem = document.getElementById('namebest');
+    elem.parentNode.removeChild(elem);
+    return false;
+
+}
+/*jshint ignore:end */
+
+//Gibt an wer gerade am Zug ist über der Tabelle 
+function changetext() {
+    if (currentPlayer === ("X")) {
+        document.getElementById("update").innerHTML = player2name + " ist als nächstes dran!";
+    } else if (currentPlayer === ("O")) {
+        document.getElementById("update").innerHTML = player1name + " ist als nächstes dran!!";
+    }
+}
+
+//wird in generate ausgeführt aber dieser ist im ignore
+//Rundenzähler fängt bei 0 an mit let round - hierdurch ändert sich der Text und zählt hoch
+function rundenzaehler() {
+    round++;
+    document.getElementById("rundenzaehlerid").innerHTML = "RUNDE " + round;
+}
+
+
+//Gewinnanzeige welches die Gewinnausgabe ausgibt
+function gewinnanzeige() {
+    if (currentPlayer === ("X")) {
+        document.getElementById("gewinnausgabe").innerHTML = player1name + " hat GEWONNEN!!!";
+    } else {
+        document.getElementById("gewinnausgabe").innerHTML = player2name + " hat GEWONNEN";
+    }
+}
+
+//ändert Buttontext um eine neue Runde zu starten
+function neustart() {
+    document.getElementById("erstellen").innerHTML = "Neue Runde starten";
+}
+
+//funktion schließt die td's damit man nicht mehr spielen kann
+function close() {
+    $("td").prop("disabled", true);
+}
+
+//generate() wird auf der HTML genutzt
+/*jshint ignore:start */
+function generate() {
+
+    let counter = 0;
+    let newId = 0;
+
+    rundenzaehler();
+    document.getElementById("gewinnausgabe").innerText = "";
+
+    $("tr").remove();
+    Spielfeldgröße = Number(document.getElementById("groeße").value); /*Feld ID hinzufügen*/
+    for (let i = 0; i < Spielfeldgröße; i++) {
+        newId = "Zeile" + i /*ID vergabe durch Zeile und i*/
+        $("table").append("<tr id=" + newId + "></tr>");
+        console.log("erstelle table")
+
+        status[i] = []; //erzeugt neue Zeile
+
+        for (let j = 0; j < Spielfeldgröße; j++) {
+            let idvar = String(i) + "-" + String(j)
+            $("#" + newId).append("<td id=" + idvar + "></td>");
+            counter++;
+            console.log("erstelle table ")
+
+            status[i][j] = " ";
+        }
+
+    }
+
+
+    console.log($("td"))
+
+}
+/*jshint ignore:end */
+
+//onclick funktion um zu Spielen - Spielablauf wird hier bestimmt 
+$("table").on("click", "td", function() {
+    if ($(this).text() === "") {
+
+        console.log($(this).attr("id").split("-")[1]);
+
+        //splitte hier die ID damit man x & y nutzen kann
+        let x = Number($(this).attr("id").split("-")[0]);
+        let y = Number($(this).attr("id").split("-")[1]);
+
+        //xy vom aktuellem spieler wird abgerufen
+        status[x][y] = currentPlayer;
+
+        $(this).text(currentPlayer);
+        console.log($(this).text());
+
+        //funktionen
+        changetext();
+        checkForWin();
+        currentPlayer = changeplayer();
+
+    }
+
+
+});
+
+/*Spielerwechsel funktion - wenn Player1, dann Player2 ansonsten Player1*/
+function changeplayer() {
+    if (currentPlayer === player1) {
+
+        currentPlayer = player2;
+    } else {
+
+        currentPlayer = player1;
+    }
+    return currentPlayer;
+
+}
+//Zählt den Wincounter für die einzelnen Spieler hoch mit der IF abfrage - so wird der aktuelle Spieler ausgesucht und der Punkt dann 
+//gegeben
+function counter() {
+    if (currentPlayer === player1) {
+        player1counter++;
+        document.getElementById("zaehler1").innerHTML = player1counter + " WIN";
+    } else if (currentPlayer === player2) {
+        player2counter++;
+        document.getElementById("zaehler2").innerHTML = player2counter + " WIN";
+    }
+}
+
+function checkForWin() {
+    wincheckhorizontalvertical();
+    wincheckquer();
+
+}
+
+function wincheckhorizontalvertical() {
+    let symbolzahl = document.getElementById("winsize").value;
+    console.log(symbolzahl);
+    /*"horizontal" und "vertikal" werden mit "X" und "O" befüllt.
+       Daraufhin wird überprüft ob die anzahl m (Symbolanzahl) übereinstimmt und dann gibt es ein Gewinner*/
+    let horizontal = "";
+    let vertikal = "";
+    let victoryOne = "O".repeat(symbolzahl);
+    let victoryTwo = "X".repeat(symbolzahl);
+    for (let i = 0; i < Spielfeldgröße; i++) {
+        horizontal = "";
+        vertikal = "";
+        for (let t = 0; t < Spielfeldgröße; t++) {
+            horizontal += status[i][t];
+            vertikal += status[t][i];
+        }
+        if (horizontal.includes(victoryOne) || vertikal.includes(victoryOne)) {
+            gewinnanzeige();
+            neustart();
+            close();
+            buttonaktivieren();
+            counter();
+
+        } else if (horizontal.includes(victoryTwo) || vertikal.includes(victoryTwo)) {
+            gewinnanzeige();
+            neustart();
+            close();
+            buttonaktivieren();
+            counter();
+        }
+    }
+}
+
+function wincheckquer() {
+    let symbolzahl = document.getElementById("winsize").value;
+    let victoryOne = "O".repeat(symbolzahl);
+    let victoryTwo = "X".repeat(symbolzahl);
+    let diagonalEins = "";
+    let diagonalZwei = "";
+    for (let i = 0; i < Spielfeldgröße; i++) {
+        for (let h = 0; h < Spielfeldgröße; h++) {
+            diagonalEins = "";
+            diagonalZwei = "";
+            if (i + (symbolzahl - 1) < Spielfeldgröße) {
+                if (h + (symbolzahl - 1) < Spielfeldgröße) {
+                    for (let m = 0; m < symbolzahl; m++) {
+                        diagonalEins += status[i + m][h + m];
+                    }
+                    if (diagonalEins.includes(victoryOne)) {
+                        gewinnanzeige();
+                        neustart();
+                        close();
+                        buttonaktivieren();
+                        counter();
+
+
+                    } else if (diagonalEins.includes(victoryTwo)) {
+                        gewinnanzeige();
+                        neustart();
+                        close();
+                        buttonaktivieren();
+                        counter();
+                    }
+                }
+            }
+            if (h - (symbolzahl - 1) >= 0) {
+                if (i + (symbolzahl - 1) < Spielfeldgröße) {
+                    for (let m = 0; m < symbolzahl; m++) {
+                        diagonalZwei += status[i + m][h - m];
+                    }
+                    if (diagonalZwei.includes(victoryOne)) {
+                        gewinnanzeige();
+                        neustart();
+                        close();
+                        buttonaktivieren();
+                        counter();
+
+                    } else if (diagonalZwei.includes(victoryTwo)) {
+                        gewinnanzeige();
+                        neustart();
+                        close();
+                        buttonaktivieren();
+                        counter();
+                    }
+                }
+            }
+        }
+    }
+}
